d2345239e08523eff9ea42d26f7a892040bcd45c
Gowsigan A <e18115@eng.pdn.ac.lk>
2023-06-08 01:30:16 +0530

    Add over the air function for esp32.

--- code/hardware/firmware/src/main.cpp
+++ code/hardware/firmware/src/main.cpp
@@ -1,9 +1,46 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const char *ssid = "Galaxy A51 53CE";
+const char *password = "00000003";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Initialize WiFi
+void initWiFi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  initWiFi();
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/plain", "Hi! I am ESP32."); });
+
+  AsyncElegantOTA.begin(&server); // Start ElegantOTA
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void)
+{
+  AsyncElegantOTA.loop();
 }
